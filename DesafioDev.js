632a471653fd507aee07afdf0b0b4de0fc22e2d0
Thales971 <thales.t.silva6@aluno.senai.br>
2025-05-30 12:01:41 -0300

    feat: Adicionar duas pessoas a lista de chamada DesavioDev.js

--- DesafioDev.js
+++ DesafioDev.js
@@ -1,4 +1,3 @@
-
 // Cadastro de novos produtos
 
 let promocoes = [
@@ -23,20 +22,18 @@ console.table(promocoes);
 
 console.log("-------------------------");
 
-
 // Adicionar itens ao cardapio
 let cardapio = [
-    "pizza de calabresa",
-    "lasanha",
-    "sushi",
-    "salada Caesar",
-    "pastel",
-    "yakisoba",
-    "feijoada"
-  ];
+  "pizza de calabresa",
+  "lasanha",
+  "sushi",
+  "salada Caesar",
+  "pastel",
+  "yakisoba",
+  "feijoada",
+];
 
-
-  console.log("Antes do push");
+console.log("Antes do push");
 console.table(cardapio);
 
 cardapio.push("hambúrguer artesanal");
@@ -50,16 +47,15 @@ console.log("---------------");
 //Último produto com defeito
 
 let producao = [
-    "mousepad",
-    "placa de vídeo",
-    "fonte de alimentação",
-    "HD externo",
-    "notebook gamer",
-    "roteador"
-  ];
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador",
+];
 
-  console.log
-("Antes do pop");
+console.log("Antes do pop");
 console.table(producao);
 
 // Removendo produto
@@ -68,44 +64,38 @@ let produtoRemovido = producao.pop();
 console.log("Depois do pop");
 console.table(producao);
 
-console.log
-("Produto Removido:", produtoRemovido)
+console.log("Produto Removido:", produtoRemovido);
 
 console.log(`-------------------------`);
 
 //Cancelamento de última inscrição
- 
-let inscritos = [
-    "Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"
-  ];
 
-  console.log
-("Antes do pop");
+let inscritos = ["Pedro", "Larissa", "João", "Amanda", "Lucas", "Mariana"];
+
+console.log("Antes do pop");
 console.table(inscritos);
 
-// Removendo ultimo 
+// Removendo ultimo
 let pessoaRemovidoo = inscritos.pop();
 
 console.log("Depois do pop");
 console.table(inscritos);
 
-console.log
-(" Pessoa removida:", pessoaRemovidoo)
+console.log(" Pessoa removida:", pessoaRemovidoo);
 
 console.log(`--------------------`);
 
 // 5 – Erro de importação
 
 let importados = [
-    "carregador portátil",
-    "cabo HDMI",
-    "leitor biométrico",
-    "controle sem fio",
-    "webcam 4K"
-  ];
+  "carregador portátil",
+  "cabo HDMI",
+  "leitor biométrico",
+  "controle sem fio",
+  "webcam 4K",
+];
 
-  console.log
-("Antes do Shift");
+console.log("Antes do Shift");
 console.table(importados);
 
 // Removendo o primeiro estilo
@@ -114,68 +104,63 @@ let importadosRemovido = importados.shift();
 console.log("Depois do shift");
 console.table(importados);
 
-console.log
-("Importado Removido:", importadosRemovido)
+console.log("Importado Removido:", importadosRemovido);
 
 console.log(`-------------------`);
 
 //– Primeira mensagem indevida
 
 let mensagens = [
-    "Propaganda indevida",
-    "Bom dia, posso ajudar?",
-    "Preciso de suporte técnico",
-    "Claro, qual o problema?",
-    "Erro ao acessar conta"
-  ];
+  "Propaganda indevida",
+  "Bom dia, posso ajudar?",
+  "Preciso de suporte técnico",
+  "Claro, qual o problema?",
+  "Erro ao acessar conta",
+];
 
-  console.log
-("Antes do Shift");
+console.log("Antes do Shift");
 console.table(mensagens);
 
-// Removendo o primeiro 
+// Removendo o primeiro
 let mensagemRemovida = mensagens.shift();
 
 console.log("Depois do shift");
-console.table(mensagens)
+console.table(mensagens);
 
-console.log
-("Mensagem Removido:", mensagemRemovida)
+console.log("Mensagem Removido:", mensagemRemovida);
 
 console.log(`-----------------`);
 
 //  Prioridade na fila
 
 let filaAtendimento = [
-    "cliente 003",
-    "cliente 004",
-    "cliente 005",
-    "cliente 006",
-    "cliente 007"
-  ];
-  
-  console.log
-("Antes do UnShift");
+  "cliente 003",
+  "cliente 004",
+  "cliente 005",
+  "cliente 006",
+  "cliente 007",
+];
+
+console.log("Antes do UnShift");
 console.table(filaAtendimento);
 
-filaAtendimento.unshift("cliente 002")
-filaAtendimento.unshift("cliente 001")
-
+filaAtendimento.unshift("cliente 002");
+filaAtendimento.unshift("cliente 001");
 
 console.log("Depois do Unshift");
 console.table(filaAtendimento);
 
 console.log(`---------------------`);
 
+// – Alunos transferidos
 
+let chamada = ["Carlos", "Fernanda", "João", "Mariana", "Lucas", "Camila"];
 
+console.log("Antes do UnShift chamada");
+console.table(chamada);
 
+filaAtendimento.unshift("Beatriz");
+filaAtendimento.unshift("Rafaela");
 
-
-
-
-  
-
-
-  
-
+console.log("Depois do Unshift chamada");
+console.table(chamada);
