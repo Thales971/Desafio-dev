a92ccaed7ebfc8d8407dfdaa095051564a4c0370
Thales971 <thales.t.silva6@aluno.senai.br>
2025-06-01 17:13:34 -0300

    feat: 24 – Ranking de vendas no arquivo DesafioDev.js

--- DesafioDev.js
+++ DesafioDev.js
@@ -456,6 +456,16 @@ console.log("Candidatos em ordem alfabética:", alfabetica);
 
 console.log(`---------------------`);
 
+//24 – Ranking de vendas
+
+let vendas = [3500, 1800, 4200, 3900, 5000, 1200, 2900, 4700];
+
+let listaOrdenada = vendas.sort((b, a) => a - b);
+
+console.log(listaOrdenada);
+
+console.log(`---------------------`);
+
 
 
 
