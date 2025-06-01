ffc658398b1eb5ef3ea83b0e32ea16f866429482
Thales971 <thales.t.silva6@aluno.senai.br>
2025-06-01 17:04:33 -0300

    feat: 22 – Alunos com média acima de 7 no arquivo DesafioDev.js

--- DesafioDev.js
+++ DesafioDev.js
@@ -421,3 +421,18 @@ let produtosAcimaDasUnidades = estoquess.filter(item => item.quantidade > 10);
 
 console.log(`---------------------`);
 
+// 22 – Alunos com média acima de 7
+
+let alunos = [
+  { nome: "Carlos", media: 6.5 },
+  { nome: "Fernanda", media: 7.2 },
+  { nome: "Lucas", media: 8.1 },
+  { nome: "Marina", media: 5.9 },
+  { nome: "João", media: 7.0 },
+  { nome: "Aline", media: 9.3 }
+];
+let alunosAprovados = alunos.filter(aluno => aluno.media >= 7);
+console.log("Alunos com a média acima de 7:", alunosAprovados);
+
+console.log(`---------------------`);
+
