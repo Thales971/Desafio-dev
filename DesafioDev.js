ce1182feae8d0d97e0338ad92a31b449036f961c
Thales971 <thales.t.silva6@aluno.senai.br>
2025-06-01 16:58:49 -0300

    feat:  21 – Filtrar itens acima do estoque mínimo no arquivo DesafioDev.js

--- DesafioDev.js
+++ DesafioDev.js
@@ -405,6 +405,19 @@ console.log(reais)
 
 console.log(`---------------------`);
 
+//21 – Filtrar itens acima do estoque mínimo
 
+let estoquess = [
+  { produto: "Teclado", quantidade: 5 },
+  { produto: "Mouse", quantidade: 12 },
+  { produto: "Monitor", quantidade: 3 },
+  { produto: "Notebook", quantidade: 18 },
+  { produto: "Impressora", quantidade: 9 },
+  { produto: "Webcam", quantidade: 20 }
+];
 
+let produtosAcimaDasUnidades = estoquess.filter(item => item.quantidade > 10);
+  console.log("Os produtos com mais de 10 unidades: ", produtosAcimaDasUnidades);  
+
+console.log(`---------------------`);
 
